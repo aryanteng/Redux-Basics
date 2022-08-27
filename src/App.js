1cdfa0a0dd9254011bbf7d9fbb4740e41b3953ce
aryanteng <aryanteng892@gmail.com>
2022-08-28 02:16:40 +0530

    redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch({ type: "INC" });
+    dispatch(actions.increment());
   };
   const decrement = () => {
-    dispatch({ type: "DEC" });
+    dispatch(actions.decrement());
   };
   const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 });
+    dispatch(actions.addBy(20));
   };
   return (
     <div>
-      shshhshs
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
